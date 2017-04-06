623f1858c7f4244fe2baa303e9b56f3abf5fa377
Karl <me@karlstoney.com>
2017-04-06 20:05:26 +0100

    Implemented prod config

--- config/index.js
+++ config/index.js
@@ -1,10 +1,14 @@
 'use strict';
 require('colors');
 const util = require('../lib/util');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
 const validateEnv = (key, warnOnly) => {
   if(!process.env[key]) {
     if(warnOnly) {
-      console.error('Warning'.yellow + ' ' + key + ' should really be set!');
+      console.warn('Warning'.yellow + ' ' + key + ' should really be set!');
       return null;
     } else {
       console.error('Error'.red + ' ' + key + ' is a required environment variable!');
@@ -34,4 +38,12 @@ config.github.clientid = validateEnv('HE_GITHUB_CLIENTID');
 config.github.clientsecret = validateEnv('HE_GITHUB_SECRET');
 config.github.callbackUrl = `${callbackUrl()}/oauth/github/callback`;
 config.port = 5000;
+
+const envConfigFile = `./${process.env.APP_ENV}.js`;
+if(process.env.APP_ENV && fs.existsSync(path.join(__dirname, envConfigFile))) {
+  const envConfig = require(envConfigFile);
+  config = _.merge(config, envConfig);
+} else {
+  console.warn('Warning'.yellow + ' no environmental configuration found for environment: ' + process.env.APP_ENV);
+}
 module.exports = config;
