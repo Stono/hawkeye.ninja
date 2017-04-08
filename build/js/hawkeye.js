df9c3c890399b575b37b65f86337b1d9d31ac888
Karl <me@karlstoney.com>
2017-04-08 12:38:57 +0100

    Added login/logout

---
+++ build/js/hawkeye.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.sparkbar').each(function () {
+    var $this = $(this);
+    $this.sparkline('html', {
+      type: 'bar',
+      height: $this.data('height') ? $this.data('height') : '30',
+      barColor: $this.data('color')
+    });
+  });
+});
