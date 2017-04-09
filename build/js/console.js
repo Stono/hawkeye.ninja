dc7712f3e62d1e119200d0f9891154d5ce18aec7
Karl <me@karlstoney.com>
2017-04-09 17:27:29 +0100

    Started work on the socket.io implementation

--- build/js/console.js
+++ build/js/console.js
@@ -5,3 +5,18 @@ var editor = CodeMirror.fromTextArea(document.getElementById('console'), {
   matchBrackets: true,
   readOnly: true
 });
+
+$(document).ready(function() {
+  socket = io();
+  socket.on('connect', function() {
+    console.log('connected');
+    socket.emit('streamLogs', window.location.pathname);
+  })
+  .on('error', function(err) {
+   console.log(err);
+  });
+
+  socket.on('log', function(msg) {
+    console.log(msg);
+  });
+});
