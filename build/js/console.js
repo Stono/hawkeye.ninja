6671db4a467ad121b7ea080a23bca61710dafb74
Karl <me@karlstoney.com>
2017-04-09 18:10:18 +0100

    Stream socket.io to codemirror

--- build/js/console.js
+++ build/js/console.js
@@ -6,6 +6,18 @@ var editor = CodeMirror.fromTextArea(document.getElementById('console'), {
   readOnly: true
 });
 
+function updateCodeMirror(data){
+  var cm = $('.CodeMirror')[0].CodeMirror;
+  var doc = cm.getDoc();
+  var cursor = doc.getCursor(); // gets the line number in the cursor position
+  var line = doc.getLine(cursor.line); // get the line contents
+  var pos = { // create a new object to avoid mutation of the original selection
+    line: (doc.size+5),
+    ch: line.length - 1 // set the character position to the end of the line
+  };
+  doc.replaceRange('\n'+data, pos); // adds a new line
+}
+
 $(document).ready(function() {
   socket = io();
   socket.on('connect', function() {
@@ -13,10 +25,10 @@ $(document).ready(function() {
     socket.emit('streamLogs', window.location.pathname);
   })
   .on('error', function(err) {
-   console.log(err);
+    console.log(err);
   });
 
   socket.on('log', function(msg) {
-    console.log(msg);
+    updateCodeMirror(msg);
   });
 });
