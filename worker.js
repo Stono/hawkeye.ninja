6cdcaa74ee300a2aed149b6be16c2c7f16b59ee1
Karl <me@karlstoney.com>
2017-04-15 14:16:11 +0100

    Fixed the worker

--- worker.js
+++ worker.js
@@ -1,12 +1,9 @@
 'use strict';
 const Worker = require('./lib/worker');
-const Redis = require('./lib/redis');
 const Dal = require('./lib/dal');
-const config = require('./config');
 const debug = require('debug')('hawkeye:worker');
 
-const redis = new Redis(config.redis);
-const dal = new Dal({ redis: redis });
+const dal = new Dal();
 const worker = new Worker({
   dal: dal
 });
