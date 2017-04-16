6cc39c1381e7571587653efddee4c6b1bf907881
Karl <me@karlstoney.com>
2017-04-16 19:54:15 +0100

    WIP acceptance

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
 const App = require('./lib/app');
 const config = require('./config');
-const app = new App(config);
+const Dal = require('./lib/dal');
+const dal = new Dal();
+
+const passport = new require('./lib/passport')({
+  clientid: config.github.clientid,
+  secret: config.github.clientsecret,
+  callbackUrl: config.github.callbackUrl,
+  authorizationURL: config.github.authorizationURL,
+  tokenURL: config.github.tokenURL,
+  userProfileURL: config.github.userProfileURL,
+  dal: dal
+});
+const app = new App({
+  dal: dal,
+  passport: passport,
+  port: config.port
+});
+
 app.start(() => {});
--- test/acceptance.js
+++ test/acceptance.js
@@ -1,48 +1,101 @@
 'use strict';
 const App = require('../lib/app');
-const config = require('../config');
-const request = require('supertest');
 const freePort = require('find-free-port');
-const _ = require('lodash');
 const should = require('should');
-const logger = require('superagent-logger');
+const Dal = require('../lib/dal');
+const Browser = require('zombie');
+const deride = require('deride');
+const fs = require('fs');
+const path = require('path');
 
-describe('App', () => {
-  let server;
-  beforeEach(done => {
-    let modifiedConfig = _.cloneDeep(config);
+describe.skip('App', () => {
+  let server, browser, passport, auth, passthrough;
+  before(done => {
     freePort(5100, 5200, (err, port) => {
+      Browser.localhost('hawkeye.website', port);
+      browser = new Browser();
       should.ifError(err);
-      modifiedConfig.port = port;
-      server = new App(modifiedConfig);
+      const dal = new Dal();
+      const passThrough = function() {
+        return (req, res, next) => {
+          if(passthrough) { return passthrough(req, res, next); }
+          next();
+        };
+      };
+
+      passport = deride.stub(['initialize', 'session', 'authenticate']);
+      passport.setup.initialize.toDoThis(passThrough);
+      passport.setup.session.toDoThis(passThrough);
+
+      passport.setup.authenticate.toDoThis(() => {
+        return (req, res, next) => {
+          if(auth) { return auth(req, res, next); }
+          next();
+        };
+      });
+
+      server = new App({
+        dal: dal,
+        passport: passport,
+        port: port
+      });
       server.start(done);
     });
   });
-  afterEach(done => {
+  after(done => {
     server.stop(done);
   });
+  afterEach(() => {
+    auth = undefined;
+    passthrough = undefined;
+  });
+  describe.only('Not logged in', () => {
+    describe('/', () => {
+      before(done => {
+        browser.visit('/', err => {
+          if(err.message !== 'document.body.createTextRange is not a function') {
+            return done(err);
+          }
+          done();
+        });
+      });
+      it('should be successful', () => {
+        browser.assert.success();
+      });
+      it('should show the sign in with github button', () => {
+        browser.assert.elements('.btn-github');
+      });
+      // Not sure on the value here, feels like i'm testing a mock
+      describe('clicking login', () => {
+        before(done => {
+          auth = (req, res) => {
+            res.redirect('https://github.com');
+          };
+          browser.click('.btn-github', done);
+        });
+        it('should redirect me to github', () => {
+          browser
+          .assert.url('https://github.com');
+        });
+      });
+    });
+  });
+  describe.skip('Logged in', () => {
+    describe('/', () => {
+      before(done => {
+        passthrough = (req, res, next) => {
+          req.user = fs.readFileSync(path.join(__dirname, 'samples/hawkeye/user.json')).toString();
+          next();
+        };
+        browser.visit('/', done);
+      });
+      it('should be successful', () => {
+        browser.assert.success();
+      });
+      it('should show the dashboard', () => {
+        browser.assert.text('title', 'Hawkeye - Dashboard');
+      });
+    });
 
-  it('should start', done => {
-    request(server.app)
-    .get('/')
-    .use(logger)
-    .expect(200)
-    .end(done);
-  });
-  it('should redirect to github login', done => {
-    request(server.app)
-    .get('/oauth/github')
-    .use(logger)
-    .expect(302)
-    .expect('Location', new RegExp(config.github.authorizationURL))
-    .end(done);
-  });
-  it('should redirect protected pages to login', done => {
-    request(server.app)
-    .get('/repo/org/name')
-    .use(logger)
-    .expect(302)
-    .expect('Location', '/oauth/github')
-    .end(done);
   });
 });
