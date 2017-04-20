aec0471343f935587929bfe68fe1307149dd9191
Karl <me@karlstoney.com>
2017-04-20 15:48:43 +0100

    Fixed JS issues

--- build/js/hawkeye.js
+++ build/js/hawkeye.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       barColor: $this.data('color')
     });
   });
-  $(".select2").select2();
+  $('.select2').select2();
 
   var defaultOrg = $('.treeview.active')[0];
   var defaultRepo = $('.repo.active')[0];
@@ -47,11 +47,3 @@ $(document).ready(function() {
     }
   });
 });
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-37161145-3', 'auto');
-ga('send', 'pageview');
