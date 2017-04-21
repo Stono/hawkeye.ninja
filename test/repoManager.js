a785b251e033c9b5baa5d8d7880d2cfcc4e23896
Karl <me@karlstoney.com>
2017-04-21 15:09:59 +0100

    RepoManager able to detect which repos need scanning

--- test/repoManager.js
+++ test/repoManager.js
@@ -46,8 +46,8 @@ describe('Repo Manager', () => {
     });
     it('track should be idempotent', done => {
       manager.schedule(123456, {
-        freq: 'Hourly',
-        when: 'Always',
+        freq: 'hourly',
+        when: 'always',
         email: 'test@test.com',
         user: 123456
       }, err => {
@@ -55,8 +55,8 @@ describe('Repo Manager', () => {
         manager.track(repo, err => {
           should.ifError(err);
           manager.get(123456, (err, data) => {
-            should(data.schedule.freq).eql('Hourly');
-            should(data.schedule.when).eql('Always');
+            should(data.schedule.freq).eql('hourly');
+            should(data.schedule.when).eql('always');
             should(data.schedule.email).eql('test@test.com');
             should(data.schedule.user).eql(123456);
             should(data.schedule.last).eql(null);
@@ -74,15 +74,15 @@ describe('Repo Manager', () => {
     });
     it('should update the schedule frequence', done => {
       manager.schedule(123456, {
-        freq: 'Hourly',
-        when: 'Always',
+        freq: 'hourly',
+        when: 'always',
         email: 'test@test.com',
         user: 123456
       }, err => {
         should.ifError(err);
         manager.get(123456, (err, data) => {
-          should(data.schedule.freq).eql('Hourly');
-          should(data.schedule.when).eql('Always');
+          should(data.schedule.freq).eql('hourly');
+          should(data.schedule.when).eql('always');
           should(data.schedule.email).eql('test@test.com');
           should(data.schedule.user).eql(123456);
           should(data.schedule.last).eql(null);
@@ -92,10 +92,11 @@ describe('Repo Manager', () => {
         });
       });
     });
+
     it('should return a list of scheduled repos based on the given frequence', done => {
       manager.schedule(123456, {
-        freq: 'Hourly',
-        when: 'Always',
+        freq: 'hourly',
+        when: 'always',
         email: 'test@test.com',
         user: 123456
       }, () => {
@@ -107,5 +108,139 @@ describe('Repo Manager', () => {
         });
       });
     });
+
+    it('should return hourly repos that have not been scanned in the last hour', done => {
+      manager.schedule(123456, {
+        freq: 'hourly',
+        when: 'always',
+        email: 'test@test.com',
+        last: new Date().setMinutes(-61),
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(1);
+          done();
+        });
+      });
+    });
+
+    it('should not return hourly repos that have been scanned in the last hour', done => {
+      manager.schedule(123456, {
+        freq: 'hourly',
+        when: 'always',
+        email: 'test@test.com',
+        last: new Date().setMinutes(-30),
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(0);
+          done();
+        });
+      });
+    });
+
+    it('should return daily repos that have not been scanned in the last day', done => {
+      const date = new Date().setDate(new Date().getDate() - 2);
+      manager.schedule(123456, {
+        freq: 'daily',
+        when: 'always',
+        email: 'test@test.com',
+        last: date,
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(1);
+          done();
+        });
+      });
+    });
+
+    it('should not return daily repos that have been scanned in the last day', done => {
+      manager.schedule(123456, {
+        freq: 'daily',
+        when: 'always',
+        email: 'test@test.com',
+        last: new Date().setMinutes(-360),
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(0);
+          done();
+        });
+      });
+    });
+
+    it('should return weekly repos that have not been scanned in the last week', done => {
+      const date = new Date().setDate(new Date().getDate() - 12);
+      manager.schedule(123456, {
+        freq: 'weekly',
+        when: 'always',
+        email: 'test@test.com',
+        last: date,
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(1);
+          done();
+        });
+      });
+    });
+
+    it('should not return weekly repos that have been scanned in the last week', done => {
+      const date = new Date().setDate(new Date().getDate() - 5);
+      manager.schedule(123456, {
+        freq: 'weekly',
+        when: 'always',
+        email: 'test@test.com',
+        last: date,
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(0);
+          done();
+        });
+      });
+    });
+
+    it('should return monthly repos that have been not scanned in the last month', done => {
+      const date = new Date().setDate(new Date().getDate() - 40);
+      manager.schedule(123456, {
+        freq: 'monthly',
+        when: 'always',
+        email: 'test@test.com',
+        last: date,
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(1);
+          done();
+        });
+      });
+    });
+
+    it('should not return monthly repos that have been scanned in the last month', done => {
+      const date = new Date().setDate(new Date().getDate() - 20);
+      manager.schedule(123456, {
+        freq: 'monthly',
+        when: 'always',
+        email: 'test@test.com',
+        last: date,
+        user: 123456
+      }, () => {
+        manager.getScheduled((err, data) => {
+          should.ifError(err);
+          should(data.length).eql(0);
+          done();
+        });
+      });
+    });
+
   });
 });
