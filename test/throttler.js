88db5f9733f5ef45b06864c4622603133841ad75
Karl <me@karlstoney.com>
2017-04-29 18:54:16 +0100

    Added throttling to the GUI new scan route too

--- test/throttler.js
+++ test/throttler.js
@@ -38,7 +38,60 @@ describe('Throttler', () => {
     };
     middleware(req, res, next);
   });
-  it('Should expire requests', function(done) {
+  it('Should throttle requests based on multiple paramters', done => {
+    const middleware = throttler.middleware({
+      key: ['query.token', 'query.another'],
+      amount: 1,
+      per: 60
+    });
+    const req = {
+      query: {
+        token: 'testing',
+        another: 'thing'
+      }
+    };
+    const res = deride.stub(['sendStatus']);
+    res.setup.sendStatus.toDoThis(code => {
+      should(code).eql(429);
+      done();
+    });
+    let nextCalled = 0;
+    const next = () => {
+      nextCalled = nextCalled + 1;
+      if(nextCalled === 2) { return done(new Error('Next was called twice, it should have been throttled')); }
+      middleware(req, res, next);
+    };
+    middleware(req, res, next);
+  });
+  it('Should not throttle when the keys arent all present', function(done) {
+    this.timeout(2000);
+    const middleware = throttler.middleware({
+      key: ['query.token', 'query.missing'],
+      amount: 1,
+      per: 60
+    });
+    const req = {
+      query: {
+        token: 'testing'
+      }
+    };
+    const res = deride.stub(['sendStatus']);
+    res.setup.sendStatus.toDoThis(code => {
+      return done(new Error('Code: ' + code + ' returned, it shouldnt have been throttled'));
+    });
+    let nextCalled = 0;
+    const next = () => {
+      nextCalled = nextCalled + 1;
+      if(nextCalled === 2) {
+        return done();
+      }
+      setTimeout(() => {
+        middleware(req, res, next);
+      }, 1001);
+    };
+    middleware(req, res, next);
+  });
+  it('Should not throttle when the "per" has expired', function(done) {
     this.timeout(2000);
     const middleware = throttler.middleware({
       key: 'query.token',
@@ -66,6 +119,4 @@ describe('Throttler', () => {
     };
     middleware(req, res, next);
   });
-
-
 });
